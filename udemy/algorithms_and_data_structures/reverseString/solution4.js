d72121c1dba807d097b7cf0dd6cc7b90ba0d29c8
Amalia <amalia.fergin@gmail.com>
2019-06-12 15:58:16 -0700

    solution 4

---
+++ udemy/algorithms_and_data_structures/reverseString/solution4.js
@@ -0,0 +1,40 @@
+
+
+
+
+// const reverse = (str) => {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed
+//   }, '')
+// }
+
+
+
+const reverse = (str) => {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
+console.log(reverse('weebee and deebee'));
+
+
+
+// The reduce() method reduces the array to a single value.
+
+
+const numbers = [1, -2, 2, 3]
+
+const reduceExample = () => {
+
+
+}
+
+
+
+
+// reduce
+
+
+
+
+
+
+//
