2e29146917fe1c5f47adb642bc4b563135eac419
Amalia <amalia.fergin@gmail.com>
2019-07-30 22:37:02 -0700

    fizzbuzz

--- udemy/algorithms_and_data_structures/palindrome/solution2.js
+++ udemy/algorithms_and_data_structures/palindrome/solution2.js
@@ -1,14 +1,19 @@
+
 const palindrome = (str) => {
   return str.split('').every((char, i) => {
     return char === str[str.length - i - 1]
-  });
+  })
 }
 
 
 
 
 
-console.log(palindrome('racecar'));
+
+
+
+
+// console.log(palindrome('racecar'));
 // console.log(palindrome('bob'));
 // console.log(palindrome('dancer'));
 
